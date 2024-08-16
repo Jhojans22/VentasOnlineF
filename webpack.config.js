7bc1407911fc9870097a0897566019d3c8c2b7db
Jhojans Velasquez <jhojansvelasquez22@gmail.com>
2024-08-15 22:07:43 -0500

    Se agregaron dependencias

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path= require('path');
+const webpack = require('webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'develoment',
+    entry: './src/client/js/index.js',
+    output:{
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                user: ['style-loader','css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template:'./src/client/index.html'
+        })
+    ]
+};
